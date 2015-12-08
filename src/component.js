962af3986694857ab71f8aa3fd3ef96af6f041a3
Leandro Lemos <leandrowd@gmail.com>
2015-12-09 01:08:19 +1100

    Adding coverage support using isparta

--- src/component.js
+++ src/component.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default class Component extends React.Component {
+  static defaultProps = {
+    foo: 1
+  };
 
+  render() {
+    return <div {...this.props}>{this.props.children}</div>;
+  }
 };
--- test/component.js
+++ test/component.js
@@ -1,7 +1,20 @@
 import { expect } from 'chai';
+import React from 'react';
+import { createRenderer } from 'react-addons-test-utils';
+import Component from '../src/component';
 
 describe('Component', () => {
+  let instance;
+
+  beforeEach(() => {
+    const shallowRenderer = createRenderer();
+    shallowRenderer.render(React.createElement(Component, {}, 'child text'));
+    instance = shallowRenderer.getRenderOutput();
+  });
+
   it('Should pass', () => {
-    expect(true).to.be.true;
+    console.log(instance);
+    expect(instance.type).to.equal('div');
+    expect(instance.props.foo).to.equal(1);
   });
 });
