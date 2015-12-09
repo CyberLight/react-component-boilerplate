9128ea67a3eb36b812e893bbd1baebf1c154471b
Leandro Lemos <leandrowd@gmail.com>
2015-12-10 00:06:12 +1100

    Sending coverage to coveralls after successfull build from travis

--- src/component.js
+++ src/component.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 export default class Component extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.node
+  }
+
   static defaultProps = {
     foo: 1
-  };
+  }
 
   render() {
     return <div {...this.props}>{this.props.children}</div>;
   }
-};
+}
